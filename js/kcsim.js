fa251cd0a6b1d718a952bb2c6353b3e93e6690ac
matlockib@gmail.com <root@Matlock-ter.localdomain>
2022-07-26 16:34:58 +0900

    DDCI (LTT) chance fix (S:D = 11:89)

--- js/kcsim.js
+++ js/kcsim.js
@@ -118,7 +118,7 @@ var NBATTACKDATA = {
 	64: { dmgMod: 1.2, accMod: 1.2, chanceMod: 1.2, id: 6, name: 'CVCI (1.2, suisei)' },
 	7: { dmgMod: 1.3, accMod: 1.5, chanceMod: 1.15, improve: 11, improveChance: .65, torpedo: true, name: 'DDCI (GTR)' },  // data: https://twitter.com/dewydrops/status/1404966491695378433
 	8: { dmgMod: 1.2, accMod: 1.65, chanceMod: 1.4, improve: 12, improveChance: .5, torpedo: true, name: 'DDCI (LTR)' },
-	9: { dmgMod: 1.5, accMod: 1.65, chanceMod: 1.25, improve: 13, improveChance: .8, torpedo: true, name: 'DDCI (LTT)' },
+	9: { dmgMod: 1.5, accMod: 1.65, chanceMod: 1.25, improve: 13, improveChance: .89, torpedo: true, name: 'DDCI (LTT)' },  // data: yumemi
 	10: { dmgMod: 1.3, accMod: 1.65, chanceMod: 1.25, improve: 14, improveChance: .55, torpedo: true, name: 'DDCI (LTD)' },
 	11: { dmgMod: 1.3, accMod: 1.5, chanceMod: 1.15, numHits: 2, torpedo: true, name: 'DDCI (GTR, double)' },
 	12: { dmgMod: 1.2, accMod: 1.65, chanceMod: 1.4, numHits: 2, torpedo: true, name: 'DDCI (LTR, double)' },
